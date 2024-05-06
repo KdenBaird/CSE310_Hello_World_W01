16b508bb7b3d1e31a8b5d9b0001e7976e0377a37
Caden Baird <bai22005@byui.edu>
2024-05-06 12:28:35 -0600

    Added calculate_rectangle_area() funciton.

--- shapes.py
+++ shapes.py
@@ -4,7 +4,13 @@
 
 def main():
     print('This is our shapes program.')
+    rectangle_area = calculate_rectangle_area(10, 20)
+    print(f'The area of rectange one is {rectangle_area} ')
+
+def calculate_rectangle_area(w, h):
+    area = w * h
+    return area
 
 
-
-main()
+if __name__ == "__main__":
+    main()
