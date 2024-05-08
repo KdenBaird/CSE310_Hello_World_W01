95e9bb0e62901f1c53b6a35d19cce90098510ccb
Caden Baird <bai22005@byui.edu>
2024-05-08 12:16:32 -0600

    added cir function and tri area function

--- shapes.py
+++ shapes.py
@@ -1,18 +1,28 @@
 # Python
 
 # This will calculate the shapes' areas. 
+import math
 
 def main():
     print('This is our shapes program.')
     rectangle_area = calculate_rectangle_area(10, 20)
-    print(f'The area of rectange one is {rectangle_area} ')
+    print(f'The area of rectange one is {rectangle_area:.2f} ')
+    triangle_area = calc_tri_area(10,20)
+    print(f'The area of triangle one is {triangle_area:.2f}')
+    circle_area = calc_circle_area(4)
+    print(f'The area of circle one is {circle_area:.2f}')
 
 def calculate_rectangle_area(w, h):
     area = w * h
     return area
 
-def cacl_tri_area(h,b):
-    b * h / b
+def calc_tri_area(h,b):
+    area = b * h / b
+    return area
+
+def calc_circle_area (r):
+    area = math.pi * r ** 2
+    return area
 
 if __name__ == "__main__":
     main()
